f363523a5129616b04ee6decbf0c68207416e5db
conda-forge <conda@conda-forge.github.io>
2018-10-12 02:00:09 +0000

    Use compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,12 @@
-#!/usr/bin/env bash
-
-# Create temporary GOPATH
-mkdir build
-export GOPATH=$(pwd)/build
-mkdir -p $GOPATH/src/github.com/terraform-providers
-
-# Link code to GOPATH directory
-ln -s $(pwd) $GOPATH/src/github.com/terraform-providers/$PKG_NAME
+# Turn the work-folder into GOPATH
+export GOPATH=${SRC_DIR}
+export PATH=${GOPATH}/bin:$PATH
+pushd src/github.com/terraform-providers/$PKG_NAME
 
 # Build
-cd $GOPATH/src/github.com/terraform-providers/$PKG_NAME
 make build
 make test
 
 # Install Binary into PREFIX/bin
+mkdir $PREFIX/bin
 mv $GOPATH/bin/$PKG_NAME $PREFIX/bin/${PKG_NAME}_v${PKG_VERSION}_x4
